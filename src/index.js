d8ecd56462650efcca2888d322790556990913c4
OleksiiKonovalchuk <108429819+OleksiiKonovalchuk@users.noreply.github.com>
2022-12-11 14:45:11 +0200

    go

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+const axios = require('axios');
+
+// Notiflix.Notify.failure(
+//   'Sorry, there are no images matching your search query. Please try again.'
+// );
